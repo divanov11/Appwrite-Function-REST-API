379b48074e932169d413c04e83963cb15d17e582
divanov11 <ivanovsin11@gmail.com>
2024-04-27 18:50:27 -0700

    -

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ const getRoutes = (request, response) => {
     response.json({ routes: ["/", "/user/:username", "/user"] });
 };
 
+const getUser = (request, response) => {
+    const { username } = request.params;
+    response.json({ username: username });
+};
+
+const createUser = (request, response) => {
+    response.json({ postData: request.body });
+};
+
 app.get("/", getRoutes);
+app.get("/user/:username", getUser);
 
 export default async (context) => await app.attach(context);
+
+// export default async ({ req, res, log, error }) => {
+//     return res.send("Hello, World!");
+// };
