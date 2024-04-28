31de2541b9b8c915ba9bbfa0b9a2beac136d1bde
divanov11 <ivanovsin11@gmail.com>
2024-04-27 18:21:20 -0700

    -

--- src/main.js
+++ src/main.js
@@ -1,33 +1,11 @@
-import { Client } from 'node-appwrite';
+import AppExpress from "@itznotabug/appexpress"
+const app = new AppExpress()
 
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+const getRoutes = (request, response) => {
+  response.json('routes': ['/', '/user/:username', '/user'])
+}
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+app.get('/', getRoutes)
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
-};
+export default async (context) => await app.attach(context) 
