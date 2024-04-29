147c4fc0325d5c2494ba7a4bba3bfba6bf1088be
divanov11 <ivanovsin11@gmail.com>
2024-04-28 17:01:14 -0700

    -

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import AppExpress from "@itznotabug/appexpress";
 const app = new AppExpress();
 import userRoutes from "./routes/user.js";
 
-app.use("/user", userRoutes);
+app.user("/user", userRoutes);
 
 const getRoutes = (request, response) => {
     response.json({ routes: ["/", "/user/:username", "/user"] });
